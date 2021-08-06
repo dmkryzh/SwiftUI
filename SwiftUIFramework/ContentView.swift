73c84c05e96065bd0ce948f8c9ecb3b5a0667851
Dmitrii Kryzhanovskii <dmkryzh@gmail.com>
2021-08-06 16:42:37 +0300

    Main tasks are done

--- SwiftUIFramework/ContentView.swift
+++ SwiftUIFramework/ContentView.swift
@@ -7,30 +7,81 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Fields: View {
     @State var loginText = ""
     @State var passwordText = ""
     
     var body: some View {
-        
-        VStack(alignment: .center, spacing: 0) {
-
-            TextField ("Login", text: $loginText)
-                .padding(.horizontal, 10)
-            VStack{
-                Color.gray.frame(height:CGFloat(1.5) / UIScreen.main.scale)
-                    .padding(.vertical, 12)
-            }
+        VStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100, alignment: .center)
             
-            SecureField ("Password", text: $passwordText)
-                .padding(.horizontal, 10)
+            VStack(alignment: .center, spacing: 0) {
+                
+                
+                TextField ("Login", text: $loginText)
+                    .padding(.horizontal, 10)
+                VStack{
+                    Color.gray.frame(height:CGFloat(1.5) / UIScreen.main.scale)
+                        .padding(.vertical, 12)
+                }
+                
+                SecureField ("Password", text: $passwordText)
+                    .padding(.horizontal, 10)
+            }
+            .frame(height: 100, alignment: .center)
+            .addBorder(Color.gray, width: 1.5,cornerRadius: 10)
+            .background(Color(.systemGray6)).cornerRadius(10)
+            .font(.system(size: 16, weight: .regular, design: .default))
+            .padding(.horizontal, 10)
         }
+    }
+}
 
-        .frame(height: 100, alignment: .center)
-        .addBorder(Color.gray, width: 1.5,cornerRadius: 10)
-        .background(Color(.systemGray6)).cornerRadius(10)
-        .padding(5)
-        .font(.system(size: 16, weight: .regular, design: .default))
+struct ContentView: View {
+    
+    
+    var body: some View {
+        
+        TabView {
+            Fields()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Feed")
+                }
+            VStack {
+                Text("Title")
+                    .bold()
+                    .background(Color.red)
+                    .font(.title)
+                    .padding()
+                Text("Some text")
+                    .padding()
+            }
+            .tabItem {
+                Image(systemName: "person.fill")
+                Text("Profile")
+            }
+            Text("Test")
+                .tabItem {
+                    Image(systemName: "music.note")
+                    Text("Player")
+                }
+            Text("Test")
+                .tabItem {
+                    Image(systemName: "play.rectangle.fill")
+                    Text("Video")
+                }
+            Text("Test")
+                .tabItem {
+                    Image(systemName: "mic.fill")
+                    Text("Recorder")
+                }
+            
+        }
+        
     }
     
 }
@@ -43,9 +94,9 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 extension View {
-     public func addBorder<S>(_ content: S, width: CGFloat = 1, cornerRadius: CGFloat) -> some View where S : ShapeStyle {
-         let roundedRect = RoundedRectangle(cornerRadius: cornerRadius)
-         return clipShape(roundedRect)
-              .overlay(roundedRect.strokeBorder(content, lineWidth: width))
-     }
- }
+    public func addBorder<S>(_ content: S, width: CGFloat = 1, cornerRadius: CGFloat) -> some View where S : ShapeStyle {
+        let roundedRect = RoundedRectangle(cornerRadius: cornerRadius)
+        return clipShape(roundedRect)
+            .overlay(roundedRect.strokeBorder(content, lineWidth: width))
+    }
+}
